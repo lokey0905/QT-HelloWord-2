d67456666a2e355b11fdbc4b178da04627d500a9
lokey0905 <lokey0905@gmail.com>
2025-10-15 16:08:44 +0800

    Add QHBoxLayout to Widget

--- widget.cpp
+++ widget.cpp
@@ -1,12 +1,17 @@
 #include "widget.h"
 
 #include <QPushButton>
+#include <QHBoxLayout>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
-
-    helloButton = new QPushButton(QPushButton("&Hello 您好",this));
+    helloButton = new QPushButton("&Hello 您好");
+    
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(helloButton);
+    
+    setLayout(layout);
 }
 
 Widget::~Widget() {}
